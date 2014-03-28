b0543dc49adddea70bc480eee51fd421e264430c
killbotlogic <me@killbotlogic.com>
2014-03-28 07:06:57 +0000

    laggy connection

--- controllers/default.js
+++ controllers/default.js
@@ -1,12 +1,27 @@
 exports.install = function(framework) {
-    framework.route('/', view_homepage);
+    
+    var self = this;
+    
+    framework.route('/', view_home);
     framework.route('/{link}/', view_detail);
+    framework.route('/cv/', view_cv);
+    framework.route('/portfolio/', view_portfolio);
+    framework.route('/projects/', view_projects);
+    
+   /* self.sitemap('Home', '/');
+    self.sitemap('CV', '/cv/');
+	self.sitemap('Portfolio', '/portfolio/');
+	self.sitemap('Projects', '/projects/');*/
+    
+    
 };
  
 // Blog list
-function view_homepage() {
+function view_home() {
     var self = this;
-    self.view('homepage');
+ 
+    //self.sitemap('Home', '/');
+    self.view('home');
 }
  
 // Blog detail
@@ -23,4 +38,22 @@ function view_detail(link) {
     }
  
     self.view('detail', blog);
+}
+
+function view_portfolio() {
+    var self = this;
+    self.view('portfolio');
+}
+
+function view_projects() {
+    var self = this;
+    self.view('projects');
+}
+
+function view_cv() {
+    var self = this;
+    
+    
+    // self.layout('_layout_new');
+    self.view('cv');
 }
