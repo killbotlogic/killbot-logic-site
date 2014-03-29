c5c0039c36b25cce8d60125ec107ea3f92844268
killbotlogic <me@killbotlogic.com>
2014-03-29 19:10:37 +1100

    commit another thing

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var framework = require('total.js');
 var http = require('http');
- 
+var os = require('os');
+
+var ip = process.argv[4] || '127.0.0.1';
+var port = process.argv[3] || '8000';
 var debug = true;
- 
-framework.run(http, debug, 8080, '0.0.0.0');
+
+
+
+framework.run(http, debug, port, ip);
